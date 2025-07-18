1b97e15cb29d41eb86a422e876ef4efcef820999
Brandon Potts <ObeliskIsHuge@gmail.com>
2025-07-18 17:24:06 -0400

    Initial Commit

---
+++ Dark Theme/Dark_ThemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dark_ThemeApp.swift
+//  Dark Theme
+//
+//  Created by Brandon Potts on 7/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Dark_ThemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
